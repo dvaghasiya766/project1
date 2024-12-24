11e3436db3eec3e23ace9011b169a82feefac066
Mr. Dev Vaghasiya <155699392+dvaghasiya766@users.noreply.github.com>
2024-12-24 13:26:32 +0530

    Hierarchy

--- src/Lab2412/Component1.jsx
+++ src/Lab2412/Component1.jsx
@@ -1,11 +1,26 @@
 import * as React from "react";
 
+class Component2 extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Component 2</h2> <Component3 />
+      </div>
+    );
+  }
+}
+class Component3 extends React.Component {
+  render() {
+    return <h3>Component 3</h3>;
+  }
+}
 class Component1 extends React.Component {
   render() {
     return (
       <div>
         <h1>Component 1</h1>
         <p>This is a simple React component.</p>
+        <Component2 />
         <button>Click Me</button>
       </div>
     );
