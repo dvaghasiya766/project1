3f6470c6ed391d30037a5647482546463864dc0b
Mr. Dev Vaghasiya <155699392+dvaghasiya766@users.noreply.github.com>
2024-12-24 13:17:00 +0530

    Initial commit

---
+++ src/Lab2412/Component1.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+class Component1 extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Component 1</h1>
+        <p>This is a simple React component.</p>
+        <button>Click Me</button>
+      </div>
+    );
+  }
+}
+export default Component1;
