2de45a7d429b3625a224935fe199b23d41f6fc8c
Mr. Dev Vaghasiya <155699392+dvaghasiya766@users.noreply.github.com>
2024-12-24 13:35:06 +0530

    Update Component1.jsx

--- src/Lab2412/Component1.jsx
+++ src/Lab2412/Component1.jsx
@@ -4,24 +4,46 @@ class Component2 extends React.Component {
   render() {
     return (
       <div>
-        <h2>Component 2</h2> <Component3 />
+        <p className="text-success">
+          <strong>This text from Component 2.</strong>
+        </p>
+        <Component3 />
       </div>
     );
   }
 }
 class Component3 extends React.Component {
   render() {
-    return <h3>Component 3</h3>;
+    return (
+      <p className="text-info">
+        <strong>This text from Component 3.</strong>
+      </p>
+    );
   }
 }
 class Component1 extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Component 1</h1>
-        <p>This is a simple React component.</p>
+      <div className="container py-5">
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+          crossorigin="anonymous"
+        ></link>
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+          crossorigin="anonymous"
+        ></script>
+        <h1 className="text-danger">Component Hierarchy</h1>
+        <p className="text-warning">
+          <strong>
+            This text from Component 1 and component1 is default class.
+          </strong>
+        </p>
         <Component2 />
-        <button>Click Me</button>
+        <button className="btn btn-primary">Click Me</button>
       </div>
     );
   }
